98d16d201e7962bbd9bf13532507bccde2751e8d
psaung <psaung299@gmail.com>
2023-09-27 16:38:17 +0700

    fix: Change db driver to postgres

--- prisma/migrations/20230927093214_init_schema/migration.sql
+++ prisma/migrations/20230927093214_init_schema/migration.sql
@@ -1,12 +1,15 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT,
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
-    "address" TEXT NOT NULL
+    "address" TEXT NOT NULL,
+    "role" TEXT NOT NULL DEFAULT 'User',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
